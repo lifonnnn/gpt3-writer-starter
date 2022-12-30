3d2e68db11faccbade259ccc316d1e9cde894c9a
lifonnnn <n.k4@outlook.com>
2022-12-30 12:03:20 +1100

    changed prompts to allow for any type of resume

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,8 +8,9 @@ const openai = new OpenAIApi(configuration);
 
 const basePromptPrefix = 
 `
-Take the following paragraph (P) which describes someones work experience and break it down into 2 parts; experience and skills. Analyse the paragraph and extrapulate the 3-4 most important tasks completed for each role. From each role analyse the tasks completed and extrapulate 2-3 skills required for each job. The resulting table should include Experience with all the jobs they've done, where they did it and, only if applicable, the dates they did it from and till, and the 3-4 most important tasks they completed as well as the skills you will extrapulate from their experience. For the 3-4 important tasks you will extrapulate don't just reword whats written in the paragraph, estimate the kind of work they have done and try and transform it into professionally written actionable tasks written in the style of a resume or CV. Separate the Experience with the Skills, but write all the skills together. List the sub tasks in order of importance.
-Paragraph:
+Interpret the following information and create a professional resume. Only include sections which can be filled out with the information provided. For example, if no information on education has been provided, do not add an education section to the resume. If information on experience has been provided, then for each role, analyse the tasks completed and extrapulate 2-3 skills required for each job and add them to a skills section. If the information states to not add a particular section, don't add it. If it states to add a section, add it.
+
+Information:
 `
 const generateAction = async (req, res) => {
     // Run first prompt
@@ -27,8 +28,7 @@ const generateAction = async (req, res) => {
     // I build Prompt #2.
     const secondPrompt = 
     `
-    Take the following headings, experience and skills and reword them to make it sound more professional in the hopes that it will entice employers to hire the person with this experience and skills. For the 3-4 sub tasks of each entry in experience, don't just simply reword them, estimate the kind of work they have done and try and transform it into professionally written actionable tasks written in the style of a resume or CV. It would be best if each point was an actionable task. Separate the Experience with the Skills, but write all the skills together. List the sub tasks in order of importance.  
-    
+    Take the following resume and reword it to make it sound more professional in the hopes that it will entice employers to hire the person with this experience and skills. For the 3-4 sub tasks of each entry in experience, don't just simply reword them, estimate the kind of work they have done and try and transform them into professionally written, actionable tasks written in the style of a resume or CV. It would be best if each point was an actionable task. List the sub tasks in order of importance.     
     ${basePromptOutput.text}
   
     Here is the resume after being reworded:
@@ -49,15 +49,13 @@ const generateAction = async (req, res) => {
 
     const thirdPrompt = 
     `
-    Here is a resume with 2 sections, Experience and Skills.
+    Here is a resume:
 
     ${secondPrompt}
 
     For entries in Skills, they should not be more than 1-2 words. If the person is proficient in a programming language, just put the language and nothing more. For example, "PHP programming" should be "PHP". Also it is preferrable to put down one word skills instead of 2-3 words. For example, "Team collaboration" can be reworded to become "communication", as without the skill of communication, team collaboration is not possible. Interpret the skills and try and reword them into 1 or maximum 2 word skills please. Thanks
 
-    Return both the Experience and Skills sections.
-
-    Resume:
+    Resume reworded:
     `
 
     const thirdPromptCompletion = await openai.createCompletion({
